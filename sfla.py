2edd1787539c66fb0ee4beaccdc22ec88cc9d664
Filiz <filiz.molla01@gmail.com>
2023-12-22 20:36:46 +0300

    sfla deneyeceğim

--- sfla.py
+++ sfla.py
@@ -1,3 +1,8 @@
+import numpy as np
+import sys, logging
+import time
+
+
 class SFLA:
     def __init__(self, frogs, mplx_no, no_of_iteration, no_of_mutation, q, sheet):
         self.frogs = frogs
@@ -32,9 +37,35 @@ class SFLA:
     
     def generate_one_frog(self):
         # Generates a frog using a best-fit heuristic and computes its score.
-        # Benimki --> Bottom Left Heurristic 
+        # Benimki --> Bottom Left Heuristic 
         
         
         
 
         return 
+    
+    
+    def generate_init_population(self):
+        """Generation of initial population
+        """
+        logger.info(f"Generating initial population (Number of frogs: {self.frogs})")
+                
+        for frog_id in range(self.frogs):
+            res_bin = self.generate_one_frog()
+            self.bins_data.bin_solutions[frog_id] = res_bin
+
+    def sort_frog(self):
+        logger.info(f"Sorting the frogs and making {self.mplx_no} memeplexes with {self.frogs} frogs each")
+        sorted_fitness = np.array(sorted(
+            self.bins_data.bin_solutions, key=lambda x: self.bins_data.bin_solutions[x].score))
+        
+        memeplexes = np.empty((self.mplx_no, self.FrogsEach))
+        for j in range(self.FrogsEach):
+            for i in range(self.mplx_no):
+                memeplexes[i, j] = sorted_fitness[i + (self.mplx_no*j)]
+        return memeplexes
+    
+    
+    
+    
+    
