4d027a9ea1030e4c248f872f4c151d8ac0ed51f7
Filiz <filiz.molla01@gmail.com>
2023-12-28 22:19:56 +0300

    OLDU. backtracking kullanarak çözüm

--- backtraking.py
+++ backtraking.py
@@ -20,6 +20,148 @@ data = """6
 3 3
 2 4"""
 
+data = """16 
+20 20
+2 12
+7 12
+8 6
+3 6
+3 5
+5 5
+3 12
+3 7
+5 7
+2 6
+3 2
+4 2
+3 4
+4 4
+9 2
+11 2"""
+
+data="""17
+20 20
+4 1
+4 5
+9 4
+3 5
+3 9
+1 4
+5 3
+4 1
+5 5
+7 2
+9 3
+3 13
+2 8
+15 4
+5 4
+10 6
+7 2"""
+
+data="""16 
+20 20
+4 14
+5 2
+2 2
+9 7
+5 5
+2 5
+7 7
+3 5
+6 5
+3 2
+6 2
+4 6
+6 3
+10 3
+6 3
+10 3"""
+
+#C2_1
+data="""28
+60 30
+7 5
+14 5
+14 8
+4 8
+21 13
+7 11
+14 11
+14 5
+4 5
+18 3
+21 3
+17 11
+4 11
+7 4
+5 4
+6 7
+18 5
+3 5
+7 3
+5 3
+18 4
+3 4
+12 2
+6 2
+18 5
+21 5
+17 3
+4 3"""
+
+#C4_2
+data="""49
+60 60
+10 14
+3 13
+28 5
+5 8
+14 9
+12 14
+13 10
+3 17
+1 5
+4 1
+18 4
+1 1 
+2 6
+4 14 
+3 18
+4 14
+8 17
+11 5
+9 12
+4 7
+25 8
+7 5
+24 9
+9 14
+12 19
+2 4
+2 7
+3 4
+5 30
+5 3
+10 26
+6 5
+4 9
+1 4
+9 2
+4 17
+5 2
+4 4
+6 2
+4 10
+2 4
+3 12
+6 5
+3 9
+7 18
+6 6
+18 7
+13 9
+25 7"""
 
 class Rectangle:
     def __init__(self, id, width, height):
@@ -133,7 +275,7 @@ class PlacementEngine:
 
     def place(self, rectangle_id, referance_rectangle_id, place, rotation):
         if(self.placements.get(rectangle_id) != None):
-            raise "Rectangle already placed."
+            raise Exception("Rectangle already placed.")
             
         rect = self.rectangles.get(rectangle_id)
         if rect == None:
@@ -206,8 +348,8 @@ class PlacementEngine:
 
     def draw(self):
         fig, ax = plt.subplots()
-        ax.set_xlim(0, self.sheet.width * 1.5)  
-        ax.set_ylim(0, self.sheet.height * 1.5)      
+        ax.set_xlim(-1, self.sheet.width * 1.5)  
+        ax.set_ylim(-1, self.sheet.height * 1.5)      
         for placement in self.placements.values():
             rect = patches.Rectangle(
                 (placement.position.x, placement.position.y),  
@@ -238,43 +380,127 @@ def readData(data):
 
 
 
-def track(rectangle_id, reference_rectangle_id, placement, rotation):
+# def track(rectangle_id, reference_rectangle_id, placement, rotation):
     
-    for placedRectangles in list(engine.placements.values()) + [None]:
-        for placement in [Place.LEFT, Place.TOP]:
-            for rotation in [Rotate.ROTATE_0, Rotate.ROTATE_90]:
-                engine.place(rectangle.id, placedRectangles, placement, rotation)
+#     for placedRectangles in list(engine.placements.values()) + [None]:
+#         for placement in [Place.LEFT, Place.TOP]:
+#             for rotation in [Rotate.ROTATE_0, Rotate.ROTATE_90]:
+#                 engine.place(rectangle_id, placedRectangles, placement, rotation)
 
-    result = engine.place(rectangle_id, reference_rectangle_id, placement, rotation)
+#     result = engine.place(rectangle_id, reference_rectangle_id, placement, rotation)
     
-    if result == True:
-        for rectnagle in engine.getUnplacedRectangles():
-            track()
-    else: 
-        return False
+#     if result == True:
+#         for rectangle in engine.getUnplacedRectangles():
+#             track()
+#     else: 
+#         return False
     
-def backtrack(sheet, rectangles):
-    engine = PlacementEngine(sheet, rectangles)
+# def backtrack(sheet, rectangles):
+#     engine = PlacementEngine(sheet, rectangles)
     
+#     for rectangle in rectangles:
+#         for placedRectangles in list(engine.placements.values()) + [None]:
+#             for placement in [Place.LEFT, Place.TOP]:
+#                 for rotation in [Rotate.ROTATE_0, Rotate.ROTATE_90]:
+#                     engine.place(rectangle.id, placedRectangles, placement, rotation)
+    
+
+
+def backtrack(engine, rectangles):
+    if len(rectangles) == 0:
+        return True
+    
+    i = 0
     for rectangle in rectangles:
-        for placedRectangles in list(engine.placements.values()) + [None]:
+        for placed in ([None] if engine.placements.__len__() == 0 else []) + list(engine.placements.keys()):
             for placement in [Place.LEFT, Place.TOP]:
                 for rotation in [Rotate.ROTATE_0, Rotate.ROTATE_90]:
-                    engine.place(rectangle.id, placedRectangles, placement, rotation)
+                    # print(rectangle, placed)
+                    result = engine.place(rectangle, placed, placement, rotation)
+                    print(engine.placements.keys(), rectangles)
+                    if i % 1000 == 0:
+                        engine.draw()
+                    i += 1
+                    
+                    if result:
+                        unplaced = engine.getUnplacedRectangles()
+                        result2 = backtrack(engine, unplaced)
+                        if result2 == False:
+                            engine.unplace(rectangle)
+                        else: 
+                            return True
+        
+  
+# def backtrack(engine, rectangles):
+#     #print(rectangles)
+#     if not rectangles:
+#         print("Solution found:")
+#         engine.draw()
+#         return True
     
+#     current_rectangle = rectangles[0]
+#     engine.place(current_rectangle.id, None, Place.TOP, Rotate.ROTATE_0)
+#     rectangles.pop(0)
+#     engine.draw()
+    
+#     #print(current_rectangle)
+
+#     for rectangle in rectangles:
+#         print(f"{rectangle}")
+#         for placed_rectangle_id, placed_rectangle in engine.placements.items():
+#             print(f"Placed: {placed_rectangle}, {placed_rectangle_id}")
+#             for placement in [Place.LEFT, Place.TOP]:
+#                 print(placement)
+#                 for rotation in [Rotate.ROTATE_0, Rotate.ROTATE_90]:
+#                      print(rotation)
+                     
+#                      result = engine.place(rectangle.id, placed_rectangle_id, placement, rotation)
+#                      engine.draw()
+#                      if result:
+#                         print(f"Placed rectangle {current_rectangle.id} successfully.")
+#                         print("Current placements:", engine.placements)
+#                         res = backtrack(engine, rectangles[1:])
+#                         if not res:
+#                             print(f"Backtracked from rectangle {current_rectangle.id}.")
+#                             engine.unplace(current_rectangle.id)
+                        
+
+def extract_from_file(file_path, file_name):
+    with open(file_path, 'r') as file:
+        data = ' \n'.join(line.strip() for line in file)
+    #data = '"""' + data + '"""'
+    return data
+   
+filename = 'C7_3'
+file_path = 'original/' + filename
+data = extract_from_file(file_path, filename)
+
+print(data)
 
 
 
-# sheet, rectangles = readData(data)
+sheet, rectangles = readData(data)
 # print(sheet, rectangles)
 
-# engine = PlacementEngine(sheet, rectangles)
-# print(engine.rectangles)
 
-# # print(engine.place(1, None, Place.TOP, Rotate.ROTATE_90))
-# # print(engine.place(3, 1, Place.LEFT, Rotate.ROTATE_90))
-# # print(engine.place(4, 1, Place.LEFT, Rotate.ROTATE_90))
-# # print(engine.place(0, 1, Place.TOP, Rotate.ROTATE_0))
+
+#rectangles = sorted(rectangles, key=lambda rect: (rect.width, rect.height), reverse=True)
+
+
+
+
+engine = PlacementEngine(sheet, rectangles)
+print(engine.rectangles)
+
+
+
+# print(engine.place(1, None, Place.TOP, Rotate.ROTATE_90))
+# print(engine.place(3, 1, Place.LEFT, Rotate.ROTATE_90))
+# print(engine.place(4, 1, Place.LEFT, Rotate.ROTATE_90))
+# print(engine.place(0, 1, Place.TOP, Rotate.ROTATE_0))
+# engine.draw()
+# print(engine.placements)
+
 
 # print(engine.place(1, None, Place.TOP, Rotate.ROTATE_90))
 # print(engine.place(3, 1, Place.TOP, Rotate.ROTATE_90))
@@ -284,8 +510,10 @@ def backtrack(sheet, rectangles):
 # engine.unplace(0)
 # engine.unplace(3)
 
-# print(engine.placements)
-# engine.draw()
 
-backtrack(sheet, rectangles)
 
+engine.draw()
+
+result = backtrack(engine, list(map(lambda x: x.id, rectangles)))
+print(result)
+print(engine.placements.keys())
